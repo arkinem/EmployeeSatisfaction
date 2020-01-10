0d2d46fc11054b5e3602c5bee1b351b6c3564f20
Blazej Golinski <blazejgolinski@live.com>
2020-01-10 01:09:26 +0000

    adds Javadoc for repositories

--- src/com/arkinem/jobrep/repository/QuestionsRepository.java
+++ src/com/arkinem/jobrep/repository/QuestionsRepository.java
@@ -8,14 +8,55 @@ import com.arkinem.jobrep.rmiinterface.Question;
 import com.arkinem.jobrep.utils.FilestringParser;
 import com.arkinem.jobrep.utils.TextFileDb;
 
+/**
+ * Repository class for questionnaire. 
+ * It uses TextFileDb to persist data.
+ * @author Blazej Golinski
+ *
+ */
 public class QuestionsRepository {
 	private List<Question> questions;
 	private TextFileDb db;
 
+	/**
+	 * Constructor creates new instance of TextFileDb.
+	 * If database is empty it will populated with one question.
+	 */
 	public QuestionsRepository() {
 		db = new TextFileDb("questions");
 		fetchData();
+		populateIfEmpty();
+	}
 
+	/**
+	 * Adds question to database
+	 * @param question question to save
+	 */
+	public void addQuestion(Question question) {
+		db.append(FilestringParser.convertQuestionToFilestring(question));
+		fetchData();
+	}
+
+	/**
+	 * Gives list of all questions
+	 * @return list of questions
+	 */
+	public List<Question> getAllQuestions() {
+		return questions;
+	}
+
+	/**
+	 * refreshes data from database
+	 */
+	private void fetchData() {
+		String fileContent = db.readAll();
+		questions = FilestringParser.convertFilestringToQuestions(fileContent);
+	}
+	
+	/**
+	 * populates database with a question if empty
+	 */
+	private void populateIfEmpty() {
 		if (questions.size() == 0) {
 			ArrayList<Answer> answers = new ArrayList<Answer>();
 			answers.add(new Answer("I strongly disagree"));
@@ -23,25 +64,12 @@ public class QuestionsRepository {
 			answers.add(new Answer("I neither agree nor disagree"));
 			answers.add(new Answer("I rather agree"));
 			answers.add(new Answer("I definetly agree"));
+			
 			String questionText = "It is easy to get information about the personnel policy in the company";
 			Question question = new Question(questionText, answers);
 
 			addQuestion(question);
 		}
-	}
-
-	public void addQuestion(Question question) {
-		db.append(FilestringParser.convertQuestionToFilestring(question));
-		fetchData();
-	}
-
-	public List<Question> getAllQuestions() {
-		return questions;
-	}
-
-	private void fetchData() {
-		String fileContent = db.readAll();
-		questions = FilestringParser.convertFilestringToQuestions(fileContent);
 	}
 
 }
--- src/com/arkinem/jobrep/repository/ResultsRepository.java
+++ src/com/arkinem/jobrep/repository/ResultsRepository.java
@@ -6,17 +6,35 @@ import java.util.UUID;
 
 import com.arkinem.jobrep.utils.TextFileDb;
 
+/**
+ * Repository class for questionnaire results. 
+ * It uses TextFileDb to persist data.
+ * @author Blazej Golinski
+ *
+ */
 public class ResultsRepository {
 	private TextFileDb db;
 
+	/**
+	 * Constructor creates new instance of TextFileDb
+	 */
 	public ResultsRepository() {
 		db = new TextFileDb("results");
 	}
 
+	/**
+	 * Submit result to the repository
+	 * @param answerId answer id (UUID)
+	 */
 	public void submitResult(UUID answerId) {
 		db.append(Arrays.asList(answerId.toString() + ";"));
 	}
 
+	/**
+	 * Get results as a list of answerId submitted.
+	 * Each id represents one vote. 
+	 * @return list of answerIds
+	 */
 	public List<String> getResults() {
 		return Arrays.asList(db.readAll().split(";"));
 	}
